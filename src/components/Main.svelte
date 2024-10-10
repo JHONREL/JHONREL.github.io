347efa51b54de2260dfeef3e4de54bb8642ea0bb
JHONREL <111577103+JHONREL@users.noreply.github.com>
2024-10-10 11:36:05 -0700

    Final

---
+++ src/components/Main.svelte
@@ -0,0 +1,233 @@
+<script>
+    import Step from "./Step.svelte";
+
+    let steps = [
+        {
+            name: "My Portfolio",
+            icon: "fa-solid fa-cart-shopping",
+            description:
+                "Smoljames Store is a a merchanising store created with Next.js, Commerce.js & Stripe! Commerce.js is a CMS for managing project and Stripe is used for all transaction handling.",
+        },
+        {
+            name: "Utlimate Todos",
+            icon: "fa-solid fa-list-check",
+            description:
+                "Test for model robustness with customized test profiles and receive exhaustive reporting on the performance and potential vulnerabilities of your model.",
+        },
+        {
+            name: "Projects",
+            icon: "fa-solid fa-diagram-project",
+            description:
+                "Augment your datasets with our filters and dataset manipulations to ensure your models are trained on the highest quality datasets (coming soon).",
+        },
+    ];
+
+    let benefits = [
+        {
+            metric: "10x",
+            name: "a self taught developer",
+            description:
+                "I taught myself to code using free online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks, backend programming, design, cloud services and much much more.",
+        },
+        {
+            name: "a product design & UX finatic",
+            description:
+                "Carefully crafting and designing amazing user experiences allows me to express and experiment with every morsel of creativity I have. I love the challenge of learning new design concepts and enabling users with amazing online experiences.",
+        },
+        {
+            name: "an excellent communicator",
+            description:
+                "Communication is key and it's a paramount value of mine. I believe in transparency and constructive communication above all else. This helps me develop deep relationships and ensures my effectiveness and productivity in any work space with any team.",
+        },
+    ];
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section
+        id="introPage"
+        class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+    >
+        <div
+            class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+        >
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! I'm <span class="poppins text-violet-400">Jhon Rel</span> Talmento
+                <br />3rd Year
+                <span class="poppins text-violet-400">Student</span>
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                I'm <span class="text-violet-400"> BSCS</span> Student
+                
+            </p>
+            <button
+                class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+            >
+                <div
+                    class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+                />
+                <h4 class="relative z-9">Get in touch &rarr;</h4>
+            </button>
+        </div>
+        <div class="relative shadow-2xl grid place-items-center">
+            <img
+                src={"images/1.jpg"}
+                alt="Zetane Engine"
+                class="object-cover z-[2] max-h-[70vh]"
+            />
+        </div>
+        <!-- <div  class="flex p-0.5 relative max-w-[700px] w-full mx-auto">
+            <div
+                class="absolute inset-0 overflow-hidden rounded-md flex items-center justify-center"
+            >
+                <div
+                    class="bg-gradient-to-r absolute inset-[-20px]  from-violet-800 to-indigo-800 specialSpin"
+                />
+            </div>
+
+            <img
+                src={"images/zetane-engine.jpeg"}
+                alt="Zetane Engine"
+                class="w-full h-full object-cover z-[2]"
+            />
+        </div> -->
+    </section>
+    <section class="py-20 lg:py-32 flex flex-col gap-24" id="projects">
+        <div class="flex flex-col gap-2 text-center">
+            <h6 class="text-large sm:text-xl md:text-2xl">
+                A few of my creative endeavors.
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                Curious to <span class="poppins text-violet-400">see</span> my work?
+            </h3>
+        </div>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+            <Step step={steps[0]}>
+                <p>
+                    My Portfolio is a a merchanising store created with <strong
+                        class="text-violet-400"
+                        >Next.js, Commerce.js, Stripe & Node.js + Express.js!</strong
+                    > Commerce.js is a product CMS and Stripe is used for all transaction
+                    handling.
+                </p>
+            </Step>
+            <Step step={steps[1]}>
+                <p>
+                    Ultimate Todos is a Full Stack <strong
+                        class="text-violet-400">Next.js</strong
+                    >,
+                    <strong class="text-violet-400">Node.js + Express.js</strong
+                    >
+                    & <strong class="text-violet-400">Firebase</strong> CRUD application
+                    that allows a user to login, manage a tidy and efficacious todo
+                    list, and persist this information across devices.
+                </p>
+            </Step>
+            <Step step={steps[2]}>
+                <p>
+                    My Work is a <strong class="text-violet-400"
+                        >SvelteKit & TailwindCSS</strong
+                    >
+                    web application, hosted on
+                    <strong class="text-violet-400">Netlify</strong>, that
+                    consumes and caches the
+                    <strong class="text-violet-400">Pokémon API</strong>
+                    to display all Pokémon information. Gotta catch them all!
+                </p>
+            </Step>
+        </div>
+    </section>
+    <section
+        id="about"
+        class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+    >
+        <!-- <div class="z-[-1] bg-violet-950 w-screen left-1/2 -translate-x-1/2 top-0 h-full absolute"> </div> -->
+        <div
+            class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+        >
+            <h6 class="text-large sm:text-xl md:text-2xl">
+                Want to know more?
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                A bit <span class="poppins text-violet-400">about</span> me.
+            </h3>
+        </div>
+        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
+            I am . . .
+        </p>
+        <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+            {#each benefits as benefit, index}
+                <!-- <div class="flex flex-col gap-2 mx-auto">
+                    <div class="flex items-end gap-4">
+                        <p
+                            class="poppins text-6xl sm:text-7xl md:text-8xl text-slate-500 font-medium"
+                        >
+                            {benefit.metric}
+                        </p>
+                        <p
+                            class="text-xl sm:text-2xl md:text-3xl capitalize pb-2"
+                        >
+                            {benefit.name}
+                        </p>
+                    </div>
+                    <p class="text-center italic">- {benefit.description}</p>
+                </div> -->
+                <div class="flex gap-6 sm:gap-8">
+                    <p
+                        class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+                    >
+                        0{index + 1}
+                    </p>
+                    <div class="flex flex-col gap-6 sm:gap-8">
+                        <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                            {benefit.name}
+                        </h3>
+                        <p>{benefit.description}</p>
+                    </div>
+                </div>
+            {/each}
+        </div>
+
+    </section>
+    <section id="contact" class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative">
+        <div class="flex flex-col gap-4 items-center justify-center">
+            <!-- <div
+                class="flex items-center gap-4 sm:gap-6 text-2xl sm:text-3xl md:text-4xl mx-auto"
+            >
+                <a>
+                    <i class="fa-brands fa-linkedin-in" />
+                </a>
+                <a>
+                    <i class="fa-brands fa-github" />
+                </a>
+            </div>
+            <p class="mx-auto text-sm opacity-70">or</p>
+            <p class="">Hugh_jass@gmail.com</p> -->
+            <p><b class="pr-2">Email</b> jhonreltalmento@gmail.com</p>
+            <p>
+                <b class="pr-2">GitHub</b>
+                <a href=" " target="_blank" class="text-violet-400"
+                    >JHONREL<sup class=""
+                        ><span class="text-xs scale-75 pl-0.5"
+                            ><i
+                                class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                            /></span
+                        ></sup
+                    ></a
+                >
+            </p>
+            <p>
+                <b class="pr-2">LinkedIn</b>
+                <a href=" " target="_blank" class="text-violet-400"
+                    >JhonRelTalmento<sup class=""
+                        ><span class="text-xs scale-75 pl-0.5"
+                            ><i
+                                class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                            /></span
+                        ></sup
+                    ></a
+                >
+            </p>
+        </div>
+    </section>
+
+</main>
